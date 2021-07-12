5228196ab154fc0a4040035205f6f5da81d7409a
Kevin Z <zkw917@gmail.com>
2021-07-12 16:02:47 -0400

    Add basic framework

--- dapp/src/App.svelte
+++ dapp/src/App.svelte
@@ -93,6 +93,7 @@
     Wallet,
     TwitterVerification,
     TwitterVerificationPublicTweet,
+    DiscordVerification,
   } from './routes';
 
   //This avoids PurgeCSS from deleting classes from production build
@@ -124,6 +125,9 @@
   </Route>
   <Route path="faq">
     <Faq />
+  </Route>
+  <Route path="discord">
+    <DiscordVerification />
   </Route>
   <Route path="twitter">
     <Twitter />
